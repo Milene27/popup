4d3ed3831e9326951e0b098fef3c9bf12f75fc87
milene <millerose22@gmail.com>
2019-10-22 13:53:05 +0200

    change color

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ $('#exampleModal').modal('show');
 
 $('#Accepter').on('click', function()
 {
-    document.location.href="TP/indexTP2.html";
+    document.location.href="TP/indexTP2.html"; /*quand on click envoie vers une nouvelle page*/
 });
